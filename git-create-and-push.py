d69511217c000ebfcc33df6f1d6c2f229b982fa6
Eric Abell <ericabell@users.noreply.github.com>
2017-07-23 19:13:51 -0500

    Initial version

---
+++ git-create-and-push.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import subprocess
+
+def main():
+    # assume we are in the directory we want to create a repo in
+
+    # get the name of the directory
+    repo_name = os.getcwd()
+
+    # run git init
+
+
+    # create a README.md file with the name of the repo,
+    # if it doesn't exist already
+
+    # git add .
+
+    # git commit -m "Initial commit"
+
+    # create the repo on github
+
+    # git remote add origin URL
+
+    # git push origin master
+
+if __name__ == "__main__":
+    main()
